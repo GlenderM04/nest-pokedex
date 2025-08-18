5374ed27f62ea1ae8b2ba415dcb90736ae8db097
Glender Miranda <gmiranda@inovacorporation.com>
2025-08-18 18:56:05 -0500

    feat: implement Axios adapter, pagination DTO, and update Pokémon service and controller for enhanced functionality

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,4 +1,22 @@
 import { Module } from '@nestjs/common';
+import { AxiosAdapter } from './adapters/axios.adapter';
 
-@Module({})
+@Module({
+  /**
+     * 
+    The providers array is used to define the services
+     (or classes) that are available within the module.
+     These services are typically injectable and can be
+     used by other components (e.g., controllers,
+    other services) within the same module.
+    */
+  providers: [AxiosAdapter],
+
+  /**
+   * The exports array specifies which providers from the
+   * current module should be made available to other modules
+   * that import this module.
+   */
+  exports: [AxiosAdapter],
+})
 export class CommonModule {}
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -4,20 +4,23 @@ import { PokeResponse } from './interfaces/pokemon-response.interface';
 import { InjectModel } from '@nestjs/mongoose';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
   ) {}
 
-  private readonly axios: AxiosInstance = axios;
+
   async executeSeed() {
     // Limpiar la colección
     await this.pokemonModel.deleteMany({});
     // Obtener los Pokémon de la API
-    const { data } = await this.axios.get<PokeResponse>(
+    // Aqui en PokeResponse estoy enviando el tipo de respuesta <T>
+    const data = await this.http.get<PokeResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=300',
     );
 
